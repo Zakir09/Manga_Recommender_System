e6b14d030e30fbfe850afcd98d747924da5bc803
Zakir <zakirahmed2002@hotmail.com>
2025-03-11 11:15:06 +0000

    Modifying layout of application for easier readability

--- app.py
+++ app.py
@@ -6,15 +6,17 @@ def recommend(manga):
     distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
     
     recommended_mangas_name = []
+    recommended_mangas_description = []
     recommended_mangas_rating = []
     recommended_mangas_tags = []
 
     for i in distances[1:9]:
         recommended_mangas_name.append(mangas.iloc[i[0]].title)
+        recommended_mangas_description.append(mangas.iloc[i[0]].description)
         recommended_mangas_rating.append(mangas.iloc[i[0]].rating)
         recommended_mangas_tags.append(mangas.iloc[i[0]].tags)
         
-    return recommended_mangas_name, recommended_mangas_tags, recommended_mangas_rating
+    return recommended_mangas_name, recommended_mangas_description, recommended_mangas_tags, recommended_mangas_rating
 
 #--- STREAMLIT UI ---
 
@@ -29,21 +31,19 @@ similarity = pickle.load(open('artifacts/similarity.pkl', 'rb'))
 manga_list = mangas['title'].values
 
 selected_manga = st.selectbox(
-    'Type or Select a manga to get recommendation',
+    'Type or Select a manga to get recommendations',
     manga_list
 )
 
 if st.button('Show Recommendation'):
-    recommended_mangas_name, recommended_mangas_tags, recommended_mangas_rating = recommend(selected_manga)
+    recommended_mangas_name, recommended_mangas_description, recommended_mangas_tags, recommended_mangas_rating = recommend(selected_manga)
 
-    st.markdown("<br>", unsafe_allow_html=True)
-    num_recommendations = len(recommended_mangas_name)
-    columns = st.columns(4) 
-    
-    for i in range(num_recommendations):
-        col = columns[i % 4]
-        with col:
+    st.markdown("---")
+
+    for i in range(len(recommended_mangas_name)):
+        with st.container():
             st.markdown(f"### {recommended_mangas_name[i]}")
-            st.markdown(f"**Tags:** {recommended_mangas_tags[i]}")
-            st.markdown(f"**Rating:** {recommended_mangas_rating[i]}")
-    st.markdown("<br><br>", unsafe_allow_html=True)
+            st.write(f"**Tags:** {recommended_mangas_tags[i]}")
+            st.write(f"**Description:** {recommended_mangas_description[i]}")
+            st.write(f"**Rating:** ⭐ {recommended_mangas_rating[i]}")
+            st.markdown("---") 
